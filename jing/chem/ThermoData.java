c716bc0b05228bb675a47120c5f935838b161165
Sally Petway <petway@mit.edu>
2005-10-09 22:30:23 +0000

    Sally added uncertainties in thermo, 10/9/05

--- jing/chem/ThermoData.java
+++ jing/chem/ThermoData.java
@@ -62,9 +62,9 @@ public class ThermoData extends ThermoGAValue {
         //#]
     }
     //## operation ThermoData(double,double,double,double,double,double,double,double,double,String)
-    public  ThermoData(double p_H298, double p_S298, double p_Cp300, double p_Cp400, double p_Cp500, double p_Cp600, double p_Cp800, double p_Cp1000, double p_Cp1500, String p_comments) {
+    public  ThermoData(double p_H298, double p_S298, double p_Cp300, double p_Cp400, double p_Cp500, double p_Cp600, double p_Cp800, double p_Cp1000, double p_Cp1500, double p_dH,double p_dS,double p_dCp,String p_comments) {
         //#[ operation ThermoData(double,double,double,double,double,double,double,double,double,String)
-        super(p_H298,p_S298,p_Cp300,p_Cp400,p_Cp500,p_Cp600,p_Cp800,p_Cp1000,p_Cp1500,p_comments);
+        super(p_H298,p_S298,p_Cp300,p_Cp400,p_Cp500,p_Cp600,p_Cp800,p_Cp1000,p_Cp1500,p_dH,p_dS,p_dCp,p_comments);
 
 
 
@@ -77,31 +77,40 @@ public class ThermoData extends ThermoGAValue {
         //#]
     }
 
+    //## operation ThermoData(String, ThermoData, String)
+    //svp
+    public ThermoData(String p_name, ThermoData p_td, String p_comments){
+      //#[ operation ThermoData(String, ThermoData, String)
+      super(p_name, p_td, p_comments);
+      //#]
+    }
+
+
     //## operation calculateCp(double)
     public double calculateCp(double p_T) {
         //#[ operation calculateCp(double)
         double T = p_T;
 
         if (300<T && T<400) {
-        	return (Cp300+(Cp400-Cp300)*(T-300.0)/100.0);
+                return (Cp300+(Cp400-Cp300)*(T-300.0)/100.0);
         }
         else if (T<500) {
-        	return (Cp400+(Cp500-Cp400)*(T-400.0)/100.0);
+                return (Cp400+(Cp500-Cp400)*(T-400.0)/100.0);
         }
         else if (T<600) {
-        	return (Cp500+(Cp600-Cp500)*(T-500.0)/100.0);
+                return (Cp500+(Cp600-Cp500)*(T-500.0)/100.0);
         }
         else if (T<800) {
-        	return (Cp600+(Cp800-Cp600)*(T-600.0)/200.0);
+                return (Cp600+(Cp800-Cp600)*(T-600.0)/200.0);
         }
         else if (T<1000) {
-        	return (Cp800+(Cp1000-Cp800)*(T-800.0)/200.0);
+                return (Cp800+(Cp1000-Cp800)*(T-800.0)/200.0);
         }
         else if (T<1500) {
-        	return (Cp1000+(Cp1500-Cp1000)*(T-1000.0)/500.0);
+                return (Cp1000+(Cp1500-Cp1000)*(T-1000.0)/500.0);
         }
         else {
-        	throw new TemperatureOutOfRangeException("Thermo: 300K~1500K;");
+                throw new TemperatureOutOfRangeException("Thermo: 300K~1500K;");
         }
         //#]
     }
@@ -145,38 +154,38 @@ public class ThermoData extends ThermoGAValue {
 
         if (T < 298) throw new TemperatureOutOfRangeException();
         if (T > 300) {
-        	if (T < 400)
-        		H += a350*(T*T-300.0*300.0)/2.0+b350*(T-300.0);
-        	else
-        		H += a350*(400*400-300.0*300.0)/2.0+b350*(400.0-300.0);
+                if (T < 400)
+                        H += a350*(T*T-300.0*300.0)/2.0+b350*(T-300.0);
+                else
+                        H += a350*(400*400-300.0*300.0)/2.0+b350*(400.0-300.0);
         }
         if (T > 400) {
-        	if (T < 500)
-        		H += a450*(T*T-400.0*400.0)/2.0+b450*(T-400.0);
-        	else
-        		H += a450*(500.0*500.0-400.0*400.0)/2.0+b450*(500.0-400.0);
+                if (T < 500)
+                        H += a450*(T*T-400.0*400.0)/2.0+b450*(T-400.0);
+                else
+                        H += a450*(500.0*500.0-400.0*400.0)/2.0+b450*(500.0-400.0);
         }
         if (T > 500) {
-        	if (T < 600)
-        		H += a550*(T*T-500.0*500.0)/2.0+b550*(T-500.0);
-        	else
-        		H += a550*(600.0*600.0-500.0*500.0)/2.0+b550*(600.0-500.0);
+                if (T < 600)
+                        H += a550*(T*T-500.0*500.0)/2.0+b550*(T-500.0);
+                else
+                        H += a550*(600.0*600.0-500.0*500.0)/2.0+b550*(600.0-500.0);
         }
         if (T > 600) {
-        	if (T < 800)
-        		H += a700*(T*T-600.0*600.0)/2.0+b700*(T-600.0);
-        	else
-        		H += a700*(800.0*800.0-600.0*600.0)/2.0+b700*(800.0-600.0);
+                if (T < 800)
+                        H += a700*(T*T-600.0*600.0)/2.0+b700*(T-600.0);
+                else
+                        H += a700*(800.0*800.0-600.0*600.0)/2.0+b700*(800.0-600.0);
         }
         if (T > 800) {
-        	if (T < 1000)
-        		H += a900*(T*T-800.0*800.0)/2.0+b900*(T-800.0);
-        	else
-        		H += a900*(1000.0*1000.0-800.0*800.0)/2.0+b900*(1000.0-800.0);
+                if (T < 1000)
+                        H += a900*(T*T-800.0*800.0)/2.0+b900*(T-800.0);
+                else
+                        H += a900*(1000.0*1000.0-800.0*800.0)/2.0+b900*(1000.0-800.0);
         }
         if (T > 1000) {
-        	if (T > 1500) throw new TemperatureOutOfRangeException();
-        	H += a1250*(T*T-1000.0*1000.0)/2.0+b1250*(T-1000.0);
+                if (T > 1500) throw new TemperatureOutOfRangeException();
+                H += a1250*(T*T-1000.0*1000.0)/2.0+b1250*(T-1000.0);
         }
 
         return H/1000.0;
@@ -185,6 +194,42 @@ public class ThermoData extends ThermoGAValue {
 
         //#]
     }
+
+    //## operation calculateGUpperBound(Temperature)
+    //svp
+    public double calculateGUpperBound(Temperature p_temperature) {
+      //#[ operation calculateGUpperBound(Temperature)
+      double T = p_temperature.getK();
+      return (calculateHUpperBound(p_temperature)*1000 - T*calculateSLowerBound(p_temperature))/1000;
+      //#]
+    }
+
+  //## operation calculateGLowerBound(Temperature)
+  //svp
+    public double calculateGLowerBound(Temperature p_temperature) {
+      //#[ operation calculateGLowerBound(Temperature)
+      double T = p_temperature.getK();
+      return (calculateHLowerBound(p_temperature)*1000 - T*calculateSUpperBound(p_temperature))/1000;
+      //#]
+    }
+
+
+    //## operation calculateHUpperBound(Temperature)
+    //svp
+      public double calculateHUpperBound(Temperature p_temperature) {
+        //#[ operation calculateHUpperBound(Temperature)
+        return calculateH(p_temperature)+Math.pow(dH,0.5);
+        //#]
+      }
+
+    //## operation calculateHLowerBound(Temperature)
+    //svp
+      public double calculateHLowerBound(Temperature p_temperature) {
+        //#[ operation calculateHLowerBound(Temperature)
+        return calculateH(p_temperature)-Math.pow(dH,0.5);
+        //#]
+      }
+
 
     //## operation calculateS(Temperature)
     public double calculateS(Temperature p_temperature) {
@@ -207,38 +252,38 @@ public class ThermoData extends ThermoGAValue {
 
         if (T < 298) throw new TemperatureOutOfRangeException();
         if (T > 300) {
-        	if (T < 400)
-        		S += a350*(T-300.0)+b350*(Math.log(T/300.0));
-        	else
-        		S += a350*(400.0-300.0)+b350*(Math.log(400.0/300.0));
+                if (T < 400)
+                        S += a350*(T-300.0)+b350*(Math.log(T/300.0));
+                else
+                        S += a350*(400.0-300.0)+b350*(Math.log(400.0/300.0));
         }
         if (T > 400) {
-        	if (T < 500)
-        		S += a450*(T-400.0)+b450*(Math.log(T/400.0));
-        	else
-        		S += a450*(500.0-400.0)+b450*(Math.log(500.0/400.0));
+                if (T < 500)
+                        S += a450*(T-400.0)+b450*(Math.log(T/400.0));
+                else
+                        S += a450*(500.0-400.0)+b450*(Math.log(500.0/400.0));
         }
         if (T > 500) {
-        	if (T < 600)
-        		S += a550*(T-500.0)+b550*(Math.log(T/500.0));
-        	else
-        		S += a550*(600.0-500.0)+b550*(Math.log(600.0/500.0));
+                if (T < 600)
+                        S += a550*(T-500.0)+b550*(Math.log(T/500.0));
+                else
+                        S += a550*(600.0-500.0)+b550*(Math.log(600.0/500.0));
         }
         if (T > 600) {
-        	if (T < 800)
-        		S += a700*(T-600.0)+b700*(Math.log(T/600.0));
-        	else
-        		S += a700*(800.0-600.0)+b700*(Math.log(800.0/600.0));
+                if (T < 800)
+                        S += a700*(T-600.0)+b700*(Math.log(T/600.0));
+                else
+                        S += a700*(800.0-600.0)+b700*(Math.log(800.0/600.0));
         }
         if (T > 800) {
-        	if (T < 1000)
-        		S += a900*(T-800.0)+b900*(Math.log(T/800.0));
-        	else
-        		S += a900*(1000.0-800.0)+b900*(Math.log(1000.0/800.0));
+                if (T < 1000)
+                        S += a900*(T-800.0)+b900*(Math.log(T/800.0));
+                else
+                        S += a900*(1000.0-800.0)+b900*(Math.log(1000.0/800.0));
         }
         if (T > 1000) {
-        	if (T > 1500) throw new TemperatureOutOfRangeException();
-        	S += a1250*(T-1000.0)+b1250*(Math.log(T/1000.0));
+                if (T > 1500) throw new TemperatureOutOfRangeException();
+                S += a1250*(T-1000.0)+b1250*(Math.log(T/1000.0));
         }
 
         return S;
@@ -248,12 +293,29 @@ public class ThermoData extends ThermoGAValue {
         //#]
     }
 
+    //## operation calculateSUpperBound(Temperature)
+    //svp
+      public double calculateSUpperBound(Temperature p_temperature) {
+        //#[ operation calculateSUpperBound(Temperature)
+        return calculateS(p_temperature)+Math.pow(dS,0.5);
+        //#]
+      }
+
+    //## operation calculateSLowerBound(Temperature)
+    //svp
+      public double calculateSLowerBound(Temperature p_temperature) {
+        //#[ operation calculateSLowerBound(Temperature)
+        return calculateS(p_temperature)-Math.pow(dS,0.5);
+        //#]
+      }
+
+
     //## operation copy()
     public ThermoData copy() {
         //#[ operation copy()
         if (this == null) return null;
 
-        return new ThermoData(H298,S298,Cp300,Cp400,Cp500,Cp600,Cp800,Cp1000,Cp1500,comments);
+        return new ThermoData(H298,S298,Cp300,Cp400,Cp500,Cp600,Cp800,Cp1000,Cp1500,dH,dS,dCp,comments);
 
 
 
@@ -286,6 +348,10 @@ public class ThermoData extends ThermoGAValue {
         Cp800 -= p_thermoData.Cp800;
         Cp1000 -= p_thermoData.Cp1000;
         Cp1500 -= p_thermoData.Cp1500;
+        dH -= Math.pow(p_thermoData.dH,2);//svp
+        dS -= Math.pow(p_thermoData.dS,2);//svp
+        dCp -= Math.pow(p_thermoData.dCp,2);//svp
+
 
 
 
@@ -306,6 +372,10 @@ public class ThermoData extends ThermoGAValue {
         Cp800 *= p_multiplier;
         Cp1000 *= p_multiplier;
         Cp1500 *= p_multiplier;
+        dH *= p_multiplier;//svp
+        dS *= p_multiplier;//svp
+        dCp *= p_multiplier;//svp
+
 
         name = name + "redundancy = " + p_multiplier + '\n';
         //#]
@@ -325,11 +395,14 @@ public class ThermoData extends ThermoGAValue {
         Cp800 += p_thermoData.Cp800;
         Cp1000 += p_thermoData.Cp1000;
         Cp1500 += p_thermoData.Cp1500;
-        //System.out.println(H298 + "  sandeep");
+        dH += Math.pow(p_thermoData.dH,2);//svp
+        dS += Math.pow(p_thermoData.dS,2);//svp
+        dCp += Math.pow(p_thermoData.dCp,2);//svp
+
 
         if (p_thermoData.getName() != null) {
-        	if (name == null) name = p_thermoData.getName() + '\n';
-        	else name = name + p_thermoData.getName() + '\n';
+                if (name == null) name = p_thermoData.getName() + '\n';
+                else name = name + p_thermoData.getName() + '\n';
         }
 
 
@@ -347,6 +420,6 @@ public class ThermoData extends ThermoGAValue {
 
 }
 /*********************************************************************
-	File Path	: RMG\RMG\jing\chem\ThermoData.java
+        File Path	: RMG\RMG\jing\chem\ThermoData.java
 *********************************************************************/
 
