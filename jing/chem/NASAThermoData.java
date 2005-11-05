0b4f9d81760c17ec2c3d92169be321c31c487b0a
Sandeep Sharma <sandeeps@mit.edu>
2005-11-05 23:40:04 +0000

    Made changes so that it calculates the thermodynamic data

--- jing/chem/NASAThermoData.java
+++ jing/chem/NASAThermoData.java
@@ -38,6 +38,9 @@ package jing.chem;
 
 import java.util.*;
 
+import jing.param.GasConstant;
+import jing.param.Temperature;
+
 //## package jing::chem 
 
 //----------------------------------------------------------------------------
@@ -48,7 +51,11 @@ import java.util.*;
 public class NASAThermoData {
   
   protected String dataString;		//## attribute dataString 
-  
+  protected double [] highTemperatureCoefficients = new double[7];
+  protected double [] lowTemperatureCoefficients = new double[7];
+  protected double lowTemperature;
+  protected double highTemperature;
+  protected double middleTemperature;
   
   // Constructors
   
@@ -56,6 +63,32 @@ public class NASAThermoData {
   public  NASAThermoData(String p_s) {
       //#[ operation NASAThermoData(String) 
       dataString = p_s;
+	  lowTemperature = Double.parseDouble(dataString.substring(48,54));
+	  highTemperature = Double.parseDouble(dataString.substring(57,64));
+	  middleTemperature = Double.parseDouble(dataString.substring(66,73));
+	  
+	  String secondLine, thirdLine, fourthLine;
+	  StringTokenizer st = new StringTokenizer(dataString,"\n");
+	  st.nextToken();
+	  secondLine = st.nextToken();
+	  thirdLine = st.nextToken();
+	  fourthLine = st.nextToken();
+	  highTemperatureCoefficients[0] = Double.parseDouble(secondLine.substring(0,15));
+	  highTemperatureCoefficients[1] = Double.parseDouble(secondLine.substring(15,30));
+	  highTemperatureCoefficients[2] = Double.parseDouble(secondLine.substring(30,45));
+	  highTemperatureCoefficients[3] = Double.parseDouble(secondLine.substring(45,60));
+	  highTemperatureCoefficients[4] = Double.parseDouble(secondLine.substring(60,75));
+	  highTemperatureCoefficients[5] = Double.parseDouble(thirdLine.substring(0,15));
+	  highTemperatureCoefficients[6] = Double.parseDouble(thirdLine.substring(15,30));
+	  
+	  lowTemperatureCoefficients[0] = Double.parseDouble(thirdLine.substring(30,45));
+	  lowTemperatureCoefficients[1] = Double.parseDouble(thirdLine.substring(45,60));
+	  lowTemperatureCoefficients[2] = Double.parseDouble(thirdLine.substring(60,75));
+	  lowTemperatureCoefficients[3] = Double.parseDouble(fourthLine.substring(0,15));
+	  lowTemperatureCoefficients[4] = Double.parseDouble(fourthLine.substring(15,30));
+	  lowTemperatureCoefficients[5] = Double.parseDouble(fourthLine.substring(30,45));
+	  lowTemperatureCoefficients[6] = Double.parseDouble(fourthLine.substring(45,60));
+	  
       //#]
   }
   public  NASAThermoData() {
@@ -70,6 +103,84 @@ public class NASAThermoData {
   
   public String getDataString() {
       return dataString;
+  }
+  
+  public Double calculateEnthalpy(Temperature temp){
+	  double T = temp.getK();
+	  double enthalpy;
+	  if (T < 298) throw new TemperatureOutOfRangeException();
+	  else if (T < middleTemperature){
+		  double [] a = new double[7];
+		  a=lowTemperatureCoefficients;
+		  enthalpy = a[0] + a[1]*T/2 + a[2]*T*T/3 + a[3]*T*T*T/4 + a[4]*T*T*T*T/5 + a[5]/T;
+		  enthalpy = enthalpy * T * GasConstant.getCalMolK()/1000;
+		  return enthalpy;
+	  }
+	  else if (T < highTemperature){
+		  double [] a = new double[7];
+		  a=highTemperatureCoefficients;
+		  enthalpy = a[0] + a[1]*T/2 + a[2]*T*T/3 + a[3]*T*T*T/4 + a[4]*T*T*T*T/5 + a[5]/T;
+		  enthalpy = enthalpy * T * GasConstant.getCalMolK()/1000;
+		  return enthalpy;
+	  }
+	  else throw new TemperatureOutOfRangeException();
+		  
+		  
+  }
+  
+  public Double calculateEntropy(Temperature temp){
+	  double T = temp.getK();
+	  double entropy;
+	  if (T < 298) throw new TemperatureOutOfRangeException();
+	  else if (T < middleTemperature){
+		  double [] a = new double[7];
+		  a=lowTemperatureCoefficients;
+		  entropy = a[0]*Math.log(T) + a[1]*T + a[2]*T*T/2 + a[3]*T*T*T/3 + a[4]*T*T*T*T/4 + a[6];
+		  entropy = entropy * GasConstant.getCalMolK();
+		  return entropy;
+	  }
+	  else if (T < highTemperature){
+		  double [] a = new double[7];
+		  a=highTemperatureCoefficients;
+		  entropy = a[0]*Math.log(T) + a[1]*T + a[2]*T*T/2 + a[3]*T*T*T/3 + a[4]*T*T*T*T/4 + a[6];
+		  entropy = entropy * GasConstant.getCalMolK();
+		  return entropy;
+	  }
+	  else throw new TemperatureOutOfRangeException();
+		  
+		  
+  }
+  
+  public Double calculateCp(Temperature temp){
+	  double T = temp.getK();
+	  double Cp;
+	  if (T < 298) throw new TemperatureOutOfRangeException();
+	  else if (T < middleTemperature){
+		  double [] a = new double[7];
+		  a=lowTemperatureCoefficients;
+		  Cp = a[0] + a[1]*T + a[2]*T*T + a[3]*T*T*T + a[4]*T*T*T*T;
+		  Cp = Cp * T;
+		  return Cp;
+	  }
+	  else if (T < highTemperature){
+		  double [] a = new double[7];
+		  a=highTemperatureCoefficients;
+		  Cp = a[0] + a[1]*T + a[2]*T*T + a[3]*T*T*T + a[4]*T*T*T*T;
+		  Cp = Cp * T;
+		  return Cp;
+	  }
+	  else throw new TemperatureOutOfRangeException();
+		  
+		  
+  }
+  
+//## operation calculateG(Temperature)
+  public double calculateFreeEnergy(Temperature p_temperature) {
+      //#[ operation calculateG(Temperature)
+      double T = p_temperature.getK();
+
+      return (calculateEnthalpy(p_temperature)*1000 - T*calculateEntropy(p_temperature))/1000;
+      //#]
   }
   
   public void setDataString(String p_dataString) {
