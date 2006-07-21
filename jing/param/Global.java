ddbbae243012c6c3ccd0014b184301bd374da6b3
Sandeep Sharma <sandeeps@mit.edu>
2006-07-21 18:16:51 +0000

    Stores variables which accessible anywhere in the code, example time in milliseconds when code was first executed.

---
+++ jing/param/Global.java
@@ -0,0 +1,8 @@
+package jing.param;
+
+public class Global {
+	public static StringBuilder diagnosticInfo = new StringBuilder();
+	public static StringBuilder enlargerInfo = new StringBuilder("Species \t singleReaction \t  doubleReaction \t longestTime \t longestTemplate \t H_Abstractiontimes \n");
+	public static long tAtInitialization;
+	public static Temperature temperature = new Temperature();
+}
