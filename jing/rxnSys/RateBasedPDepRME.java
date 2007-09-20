33fd6cf0e7504890a4d96e127750e339b8aeae82
Sandeep Sharma <sandeeps@mit.edu>
2007-09-20 19:54:38 +0000

    when pdepnetwork is updated only the species in the network is explored and is not reacted with all the rest of the species.

--- jing/rxnSys/RateBasedPDepRME.java
+++ jing/rxnSys/RateBasedPDepRME.java
@@ -61,7 +61,48 @@ public class RateBasedPDepRME implements ReactionModelEnlarger {
         //#[ operation enlargeReactionModel(ReactionSystem) 
         Object update = getNextUpdatedObject(p_reactionSystem);
         String return_string = "";
-        if (update instanceof Species) {
+        
+        if (update instanceof PDepNetwork) {
+        	PDepNetwork pnw = (PDepNetwork)update;
+        	Species nextIsomer = null;
+        	if (!pnw.isActive() && pnw.getIsChemAct()) {
+        		nextIsomer = (Species)pnw.getProduct().iterator().next();
+        	}
+        	else {
+        		double maxKLeak = 0;
+        		PDepNetReaction path = null;
+        		for (Iterator iter = pnw.getPDepNonincludedReactionList(); iter.hasNext(); ) {
+        			PDepNetReaction pdnr = (PDepNetReaction)iter.next();
+        			double kleak = pdnr.calculateRate();
+        			if (maxKLeak < kleak) {
+        				maxKLeak = kleak;
+        				path = pdnr;
+        			}
+        		}
+        		
+        		if (path == null) throw new InvalidReactionSystemUpdateException();
+        		nextIsomer = (Species)path.getProducts().next();
+        	}
+        	if (nextIsomer == null) throw new InvalidReactionSystemUpdateException();
+        	
+        		if (pnw.getIsChemAct())
+            		System.out.println("entry reaction " + pnw.getEntryReaction().toString());
+            	else {
+            		Species entry = (Species)pnw.getReactant().iterator().next();
+            		System.out.println("entry species " + entry.toString());
+            	}
+        		System.out.println("adding new isomer to the PDepNetwork: " + nextIsomer.toString());
+        		if (nextIsomer.getPdepPaths() == null){
+        			((TemplateReactionGenerator)p_reactionSystem.getReactionGenerator()).generatePdepReactions(nextIsomer);
+                	((LibraryReactionGenerator)p_reactionSystem.lrg).generatePdepReactions(nextIsomer);
+        		}
+        		pnw.update(nextIsomer);
+            	pnw.runPDepCalculation(p_reactionSystem);
+            	return;
+        	
+        	
+        }
+        if (update instanceof Species ) {
         	Species next = (Species)update;
         	CoreEdgeReactionModel cerm = (CoreEdgeReactionModel)p_reactionSystem.getReactionModel();
         	PresentStatus ps = p_reactionSystem.getPresentStatus();
@@ -77,26 +118,7 @@ public class RateBasedPDepRME implements ReactionModelEnlarger {
         	double Cp = next.calculateCp(temp);
         	System.out.println("Thermo\t" + String.valueOf(H) + '\t' + String.valueOf(S)+ '\t' + String.valueOf(G)+ '\t' + String.valueOf(Cp));
         	
-        	// print the reaction forms this species
-            System.out.println("the reactions forming this species:");
-            LinkedHashSet ur = cerm.getUnreactedReactionSet();
-            for (Iterator iter = ur.iterator(); iter.hasNext();) {
-            	Reaction r = (Reaction)iter.next();
-            	if (r.contains(next)) {
-            		double rate = r.calculateTotalRate(p_reactionSystem.getPresentTemperature());
-            		if (r instanceof TemplateReaction) {
-            			rate = ((TemplateReaction)r).calculateTotalPDepRate(p_reactionSystem.getPresentTemperature());
-            		}
-            		System.out.print(r.toString() + "\trate is " + String.valueOf(rate));
-            		for (Iterator rIter = r.getReactants(); rIter.hasNext();) {
-            			Species reactant = ((ChemGraph)rIter.next()).getSpecies();
-            			SpeciesStatus ss = ps.getSpeciesStatus(reactant);
-            			rate*=ss.getConcentration(); 
-            		}
-            		System.out.println("\tflux is " + String.valueOf(rate));
-            	}	
-            }
-            
+        	
             
         	if (cerm.containsAsReactedSpecies(next)) 
         		throw new InvalidNextCandidateSpeciesException();
@@ -106,44 +128,27 @@ public class RateBasedPDepRME implements ReactionModelEnlarger {
         	}
         	// generate new reaction set
         	LinkedHashSet newReactionSet = p_reactionSystem.getReactionGenerator().react(cerm.getReactedSpeciesSet(),next);
+        	newReactionSet.addAll(p_reactionSystem.lrg.react(cerm.getReactedSpeciesSet(),next));
         	
         	// partition the reaction set into reacted reaction set and unreacted reaction set
         	// update the corresponding core and edge model of CoreEdgeReactionModel
-        	cerm.addReactionSet(newReactionSet);
+        	
+        	Iterator iter = newReactionSet.iterator();
+        	while (iter.hasNext()){
+        		Reaction r = (Reaction)iter.next();
+        		if (r.getReactantNumber() ==2 && r.getProductNumber() == 2)
+        			cerm.addReaction(r);
+        	}
         
-        }
-        else if (update instanceof PDepNetwork) {
-        	PDepNetwork pnw = (PDepNetwork)update;
-        	Species nextIsomer = null;
-        	if (!pnw.isActive() && pnw.getIsChemAct()) {
-        		nextIsomer = ((ChemGraph)pnw.getProduct().iterator().next()).getSpecies();
-        	}
-        	else {
-        		double maxKLeak = 0;
-        		PDepNetReaction path = null;
-        		for (Iterator iter = pnw.getPDepNonincludedReactionList(); iter.hasNext(); ) {
-        			PDepNetReaction pdnr = (PDepNetReaction)iter.next();
-        			double kleak = pdnr.getRate();
-        			if (maxKLeak < kleak) {
-        				maxKLeak = kleak;
-        				path = pdnr;
-        			}
-        		}
-        		
-        		if (path == null) throw new InvalidReactionSystemUpdateException();
-        		nextIsomer = ((ChemGraph)path.getProducts().next()).getSpecies();
-        	}
-        	if (nextIsomer == null) throw new InvalidReactionSystemUpdateException();
-        	System.out.println("adding new isomer to the PDepNetwork: " + nextIsomer.toString());
-        	pnw.update(nextIsomer);
-        	pnw.runPDepCalculation(p_reactionSystem);
         }
         else throw new InvalidReactionSystemUpdateException();
         
-        return ;
+        
         
         //#]
     }
+    
+
     
     //## operation getNextUpdatedObject(ReactionSystem) 
     public Object getNextUpdatedObject(ReactionSystem p_reactionSystem) {
@@ -157,8 +162,8 @@ public class RateBasedPDepRME implements ReactionModelEnlarger {
         
         for (Iterator iter = cerm.getUnreactedSpeciesSet().iterator();iter.hasNext();) {
         	Species us = (Species)iter.next();
-        	double thisFlux = Math.abs(ps.getSpeciesStatus(us).getFlux());
-        	if (thisFlux > maxFlux) {
+        	double thisFlux = Math.abs(Math.abs(ps.unreactedSpeciesFlux[us.getID()]));
+        	if (thisFlux >= maxFlux) {
         		maxFlux = thisFlux;
         		maxSpecies = us;
         	}
@@ -172,10 +177,10 @@ public class RateBasedPDepRME implements ReactionModelEnlarger {
         	double rleak = pdn.getKLeak();
         	
             for (Iterator rIter = pdn.getReactant().iterator(); rIter.hasNext(); ) {
-        		ChemGraph cg = (ChemGraph)rIter.next();
-        		Species spe = cg.getSpecies();
+            	Species spe = (Species)rIter.next();
+        		//Species spe = cg.getSpecies();
         		double conc = 0;
-        		if (cerm.containsAsReactedSpecies(spe)) conc = ps.getSpeciesStatus(spe).getConcentration();
+        		if (cerm.containsAsReactedSpecies(spe) && ps.getSpeciesStatus(spe) != null) conc = ps.getSpeciesStatus(spe).getConcentration();
         		rleak *= conc;
         	}
         	//System.out.println("PDepNetwork " + pdn.toString() + " with RLeak: " + String.valueOf(rleak));
@@ -189,22 +194,39 @@ public class RateBasedPDepRME implements ReactionModelEnlarger {
         System.out.println(ps.getTime());
         System.out.println("Rmin: " + String.valueOf(Rmin));
         System.out.println("unreacted Spe " + maxSpecies.getName() + " with highest flux: " + String.valueOf(maxFlux));
-        System.out.println("PDepNetwork " + maxPdn.toString() + " with highest rleak: " + String.valueOf(maxRleak));
+        if (maxPdn != null)
+        	System.out.println("PDepNetwork " + maxPdn.toString() + " with highest rleak: " + String.valueOf(maxRleak));
         
+        // return max pdep network only if there is no species present
+        if (maxRleak >= Rmin && maxFlux <= Rmin) {
+        	if (maxPdn.getIsChemAct())
+        		System.out.println("entry reaction " + maxPdn.getEntryReaction().toString());
+        	else {
+        		Species entry = (Species)maxPdn.getReactant().iterator().next();
+        		System.out.println("entry species " + entry.toString());
+        	}
+        		
+        	return maxPdn;
+        	
+        }
+        else {
+        	return maxSpecies;
+        }	
         
-        if (maxFlux > Rmin && maxRleak < Rmin) {
+        // return max species if there is no incomplete pressure dependent reaction network
+        /*if (maxFlux >= Rmin && maxRleak <= Rmin) {
         	return maxSpecies;
         }
         else {
         	if (maxPdn.getIsChemAct())
         		System.out.println("entry reaction " + maxPdn.getEntryReaction().toString());
         	else {
-        		Species entry = ((ChemGraph)maxPdn.getReactant().iterator().next()).getSpecies();
+        		Species entry = (Species)maxPdn.getReactant().iterator().next();
         		System.out.println("entry species " + entry.toString());
         	}
         		
         	return maxPdn;
-        }	
+        }*/	
         //#]
     }
     
