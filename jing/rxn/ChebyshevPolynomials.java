287b69b3aaccc829d310813d7008192f2fe8f551
Sandeep Sharma <sandeeps@mit.edu>
2007-09-20 19:31:21 +0000

    Initialize chebyshev polynomials for pressure dependent reactions.

---
+++ jing/rxn/ChebyshevPolynomials.java
@@ -0,0 +1,247 @@
+//!********************************************************************************
+//!
+//!    RMG: Reaction Mechanism Generator                                            
+//!
+//!    Copyright: Jing Song, MIT, 2002, all rights reserved
+//!     
+//!    Author's Contact: jingsong@mit.edu
+//!
+//!    Restrictions:
+//!    (1) RMG is only for non-commercial distribution; commercial usage
+//!        must require other written permission.
+//!    (2) Redistributions of RMG must retain the above copyright
+//!        notice, this list of conditions and the following disclaimer.
+//!    (3) The end-user documentation included with the redistribution,
+//!        if any, must include the following acknowledgment:
+//!        "This product includes software RMG developed by Jing Song, MIT."
+//!        Alternately, this acknowledgment may appear in the software itself,
+//!        if and wherever such third-party acknowledgments normally appear.
+//!  
+//!    RMG IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED 
+//!    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+//!    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+//!    DISCLAIMED.  IN NO EVENT SHALL JING SONG BE LIABLE FOR  
+//!    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+//!    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+//!    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;  
+//!    OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  
+//!    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT  
+//!    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+//!    THE USE OF RMG, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//! 
+//!******************************************************************************
+
+
+
+package jing.rxn;
+
+
+import java.util.*;
+import jing.mathTool.*;
+import jing.param.Pressure;
+import jing.param.Temperature;
+
+//## package jing::rxn 
+
+//----------------------------------------------------------------------------
+// jing\rxn\ChebyshevPolynomials.java                                                                  
+//----------------------------------------------------------------------------
+
+//## class ChebyshevPolynomials 
+public class ChebyshevPolynomials {
+    
+    protected int NP;		//## attribute NP 
+    
+    protected int NT;		//## attribute NT 
+    
+    protected Pressure Plow;		//## attribute Plow 
+    
+    protected Pressure Pup;		//## attribute Pup 
+    
+    protected Temperature Tlow;		//## attribute Tlow 
+    
+    protected Temperature Tup;		//## attribute Tup 
+    
+    protected double [][] alpha;		//## attribute alpha 
+    
+    
+    // Constructors
+    
+    //## operation ChebyshevPolynomials(int,Temperature,Temperature,int,Pressure,Pressure,double [][]) 
+    public  ChebyshevPolynomials(int p_nT, Temperature p_Tlow, Temperature p_Tup, int p_nP, Pressure p_Plow, Pressure p_Pup, double [][] p_alpha) {
+        //#[ operation ChebyshevPolynomials(int,Temperature,Temperature,int,Pressure,Pressure,double [][]) 
+        if (p_alpha == null) throw new NullPointerException();
+        
+        if (p_nT < 0 || p_nP < 0 || p_alpha.length < p_nT || p_alpha[0].length < p_nP) throw new InvalidChebyshevPolynomialsException();
+        if (p_Tup.getK() < p_Tlow.getK() || p_Pup.getAtm() < p_Plow.getAtm()) throw new InvalidChebyshevPolynomialsException();
+         
+        NT = p_nT;
+        NP = p_nP;
+        Tup = p_Tup;
+        Tlow = p_Tlow;
+        Pup = p_Pup;
+        Plow = p_Plow;
+        
+        alpha = new double[NT][NP];
+        
+        for (int i = 0; i < NT; i++) {
+        	for (int j = 0; j < NP; j++) {
+        		alpha[i][j] = p_alpha[i][j];
+        	}
+        }
+        
+        
+        //#]
+    }
+    public  ChebyshevPolynomials() {
+    }
+    
+    //## operation calculatePAvg(Pressure) 
+    private double calculatePAvg(Pressure p_pressure) {
+        //#[ operation calculatePAvg(Pressure) 
+        double P = p_pressure.getAtm();
+        double Pmin = Plow.getAtm();
+        double Pmax = Pup.getAtm();
+        
+        double result = (2*Math.log(P)-Math.log(Pmin)-Math.log(Pmax))/(Math.log(Pmax)-Math.log(Pmin));
+        
+        return result;
+        
+        //#]
+    }
+    
+    public void addChebyshevPolynomial(ChebyshevPolynomials cbp){
+    	if (NP != cbp.NP || NT != cbp.NT || Plow.getAtm() !=cbp.Plow.getAtm() || Pup.getAtm() != cbp.Pup.getAtm() || Tup.getK() != cbp.Tup.getK() || Tlow.getK() != cbp.Tlow.getK()){
+    		System.err.println("The chebyshev polynomial: \n" + toChemkinString()+  "cannot be added directly to :\n" +cbp.toChemkinString());
+    		
+    		System.exit(0);
+    	}
+    	for (int i = 0; i < NT; i++) {
+        	for (int j = 0; j < NP; j++) {
+        		alpha[i][j] = alpha[i][j]+cbp.alpha[i][j];
+        	}
+        } 
+    }
+    
+    //## operation calculatePhi(int,double) 
+    private double calculatePhi(int p_i, double p_x) {
+        //#[ operation calculatePhi(int,double) 
+        if (p_i < 1) throw new InvalidChebyshevPolynomialsException();
+        double result = Math.cos((p_i-1)*Math.acos(p_x));
+        return result;
+        //#]
+    }
+    
+    //## operation calculateRate(Temperature,Pressure) 
+    public double calculateRate(Temperature p_temperature, Pressure p_pressure) {
+        //#[ operation calculateRate(Temperature,Pressure) 
+        if (p_temperature.getK()>Tup.getK() || p_temperature.getK()<Tlow.getK()) throw new TOutOfRangeException();
+        if (p_pressure.getAtm()>Pup.getAtm() || p_pressure.getAtm()<Plow.getAtm()) throw new POutOfRangeException();
+        
+        double Tavg = calculateTAvg(p_temperature);
+        double Pavg = calculatePAvg(p_pressure);
+        
+        double result = 0;
+        for (int i = 0; i < NT; i++) {
+        	for (int j = 0; j < NP; j++) {
+        		result += alpha[i][j]*calculatePhi(i+1, Tavg)*calculatePhi(j+1,Pavg);
+        	}
+        }
+        
+        double k = Math.pow(10,result);//changed by Sally 1/24/06
+        
+        return k;
+        //#]
+    }
+    
+    //## operation calculateTAvg(Temperature) 
+    private double calculateTAvg(Temperature p_temperature) {
+        //#[ operation calculateTAvg(Temperature) 
+        double T = p_temperature.getK();
+        double Tmin = Tlow.getK();
+        double Tmax = Tup.getK();
+        
+        double result = (2.0/T-1.0/Tmin-1.0/Tmax)/(1.0/Tmax-1.0/Tmin);
+        return result;
+        //#]
+    }
+    
+    //## operation toChemkinString() 
+    public String toChemkinString() {
+        //#[ operation toChemkinString() 
+        String result = "TCHEB / " + Tlow.getK() + " " + Tup.getK() + " /";
+        result += "\tPCHEB / " + Plow.getAtm() + " " + Pup.getAtm() + " /\n";
+        result += "CHEB / " + NT + '\t' + NP + " /\n";
+        for (int i = 0; i < NT; i++) {
+        	result += "CHEB / ";
+        	for (int j = 0; j < NP; j++) {
+        		result += alpha[i][j] + " ";
+        	}
+        	result += "/\n";
+        }
+        return result;
+        
+        //#]
+    }
+    
+    public int getNP() {
+        return NP;
+    }
+    
+    public void setNP(int p_NP) {
+        NP = p_NP;
+    }
+    
+    public int getNT() {
+        return NT;
+    }
+    
+    public void setNT(int p_NT) {
+        NT = p_NT;
+    }
+    
+    public Pressure getPlow() {
+        return Plow;
+    }
+    
+    public void setPlow(Pressure p_Plow) {
+        Plow = p_Plow;
+    }
+    
+    public Pressure getPup() {
+        return Pup;
+    }
+    
+    public void setPup(Pressure p_Pup) {
+        Pup = p_Pup;
+    }
+    
+    public Temperature getTlow() {
+        return Tlow;
+    }
+    
+    public void setTlow(Temperature p_Tlow) {
+        Tlow = p_Tlow;
+    }
+    
+    public Temperature getTup() {
+        return Tup;
+    }
+    
+    public void setTup(Temperature p_Tup) {
+        Tup = p_Tup;
+    }
+    
+    public double getAlpha(int i2, int i1) {
+        return alpha[i2][i1];
+    }
+    
+    public void setAlpha(int i2, int i1, double p_alpha) {
+        alpha[i2][i1] = p_alpha;
+    }
+    
+}
+/*********************************************************************
+	File Path	: RMG\RMG\jing\rxn\ChebyshevPolynomials.java
+*********************************************************************/
+
