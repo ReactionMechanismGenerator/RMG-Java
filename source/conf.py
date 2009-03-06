1ad7ad01dcd5cadc134a70381c82041f38fbaf7c
Greg Magoon <gmagoon@mit.edu>
2009-03-06 15:43:35 +0000

    changed rmg.mit.edu reference to rmg.sourceforge.net

--- source/conf.py
+++ source/conf.py
@@ -148,7 +148,7 @@ html_use_modindex = False
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-html_use_opensearch = 'http://rmg.mit.edu/'
+html_use_opensearch = 'http://rmg.sourceforge.net/'
 
 # If nonempty, this is the file name suffix for HTML files (e.g. ".xhtml").
 #html_file_suffix = ''
