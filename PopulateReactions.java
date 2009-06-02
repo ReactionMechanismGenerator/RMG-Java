648e7c29a44ea49e1937aea0dff7cf97c4c20ab8
Michael Harper <mrharper@mit.edu>
2009-06-02 15:34:14 +0000

    [MRH] Updated PopulateReactions.java
    Before: Empy function
    
    Now: Generates a list of all possible reactions between all species supplied in the input file.
    
    The input (.txt) file should contain a list of ChemGraphs, with the first line being a user-defined name for the species and the remaining lines containing the graph (in the form of an adjacency list).  There is no limit on the number of ChemGraphs the user may supply.  The output of the function is two .txt files: PopRxnsOutput_rxns.txt and PopRxnsOutput_spcs.txt.  The first contains the list of reactions (including the modified Arrhenius parameters and RMG-generated comments) and the second the list of species (including the ChemGraph) that are involved in the list of reactions.

--- PopulateReactions.java
+++ PopulateReactions.java
@@ -25,7 +25,148 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+import jing.chem.ChemGraph;
+import jing.chem.ForbiddenStructureException;
+import jing.chem.Species;
+import jing.chemParser.ChemParser;
+import jing.chemUtil.Graph;
+import jing.rxn.Kinetics;
+import jing.rxn.Reaction;
+import jing.rxn.TemplateReactionGenerator;
+import jing.rxnSys.CoreEdgeReactionModel;
 
 public class PopulateReactions {
+	/**
+	 * Generates a list of all possible reactions between all species supplied
+	 * 	in the input file.
+	 * The input (.txt) file should contain a list of ChemGraphs, with the first
+	 * 	line being a user-defined name for the species and the remaining lines
+	 * 	containing the graph (in the form of an adjacency list).  There is no limit
+	 * 	on the number of ChemGraphs the user may supply.
+	 * The output of the function is two .txt files: PopRxnsOutput_rxns.txt and
+	 * 	PopRxnsOutput_spcs.txt.  The first contains the list of reactions (including
+	 * 	the modified Arrhenius parameters and RMG-generated comments) and the second
+	 * 	the list of species (including the ChemGraph) that are involved in the list
+	 * 	of reactions.
+	 */
+	public static void main(String[] args) {
+		initializeSystemProperties();
+		// Define variable 'speciesSet' to store the species contained in the input file
+		LinkedHashSet speciesSet = new LinkedHashSet();
+		// Define variable 'reactions' to store all possible rxns between the species in speciesSet
+		LinkedHashSet reactions = new LinkedHashSet();
+		TemplateReactionGenerator rtLibrary = new TemplateReactionGenerator();
+		
+		// Define two string variables 'listOfReactions' and 'listOfSpecies'
+		//	These strings will hold the list of rxns (including the structure,
+		//	modified Arrhenius parameters, and source/comments) and the list of
+		//	species (including the chemkin name and graph), respectively
+		String listOfReactions = "Arrhenius 'A' parameter has units of: mol,cm3,s\n" +
+			"Arrhenius 'n' parameter is unitless\n" +
+			"Arrhenius 'E' parameter has units of: kcal/mol\n\n";
+		String listOfSpecies = "";
+		
+		// Open and read the input file
+		try {
+			FileReader fr_input = new FileReader(args[0]);
+			BufferedReader br_input = new BufferedReader(fr_input);
+			// Read in the first line of the input file
+			String line = ChemParser.readMeaningfulLine(br_input);
+			while (line != null) {
+				// The first line of a new species is the user-defined name
+				String speciesName = line;
+				// The remaining lines are the graph
+				Graph g = ChemParser.readChemGraph(br_input);
+				// Make the ChemGraph, assuming it does not contain a forbidden structure
+				ChemGraph cg = null;
+				try {
+					cg = ChemGraph.make(g);
+				} catch (ForbiddenStructureException e) {
+					System.out.println("Error in reading graph: Graph contains a forbidden structure.\n" + g.toString());
+					System.exit(0);
+				}
+				// Make the species
+				Species species = Species.make(speciesName,cg);
+				// Add the new species to the set of species
+				speciesSet.add(species);
+				// Define a dummy hash set 'new_reactions' to hold the list
+				//	of rxns for the new species against the species seed
+				LinkedHashSet new_reactions = new LinkedHashSet();
+				// React the new species with the set of species (including itself)
+				new_reactions = rtLibrary.react(speciesSet,species);
+				reactions.addAll(new_reactions);
+				// Read the next line of the input file
+				line = ChemParser.readMeaningfulLine(br_input);
+			}
+			// Iterate over all rxns, abstracting the necessary information,
+			//	and store the results in the 'listOfReactions' string
+			Iterator iter_rxns = reactions.iterator();
+        	while (iter_rxns.hasNext()){
+        		Reaction r = (Reaction)iter_rxns.next();
+        		Kinetics rxn_k = r.getKinetics();
+        		listOfReactions += r.toString()	+ "\t" + rxn_k.getAValue()
+        										+ "\t" + rxn_k.getNValue()
+        										+ "\t" + rxn_k.getEValue()
+        										+ "\t" + rxn_k.getSource()
+        										+ rxn_k.getComment()
+        										+ "\n";
+        		// Add the products of the reactions to the list of species
+        		//	hash set.  The reactants of each reaction are already
+        		//	present.  This list will allow us to generate the graphs
+        		//	for all species involved in rxns.
+        		speciesSet.addAll(r.getProductList());
+			}
+        	Iterator iter_species = speciesSet.iterator();
+        	// Define dummy integer 'i' so our getChemGraph().toString()
+        	//	call only returns the graph
+        	int i = 0;
+        	while (iter_species.hasNext()) {
+        		Species species = (Species)iter_species.next();
+        		listOfSpecies += species.getChemkinName() + "\n" +
+        			species.getChemGraph().toString(i) + "\n";
+        	}
+        	// Write the output files
+        	try{
+        		File rxns = new File("PopRxnsOutput_rxns.txt");
+        		File spcs = new File("PopRxnsOutput_spcs.txt");
+        		FileWriter fw_rxns = new FileWriter(rxns);
+        		FileWriter fw_spcs = new FileWriter(spcs);
+        		fw_rxns.write(listOfReactions);
+        		fw_rxns.close();
+        		fw_spcs.write(listOfSpecies);
+        		fw_spcs.close();
+        	}
+        	catch (IOException e) {
+            	System.out.println("Could not write PopRxnsOutput.txt files");
+            	System.exit(0);
+            }
+        	// Display to the user that the program was successful and also
+        	//	inform them where the results may be located
+			System.out.println("Reaction population complete. Results are stored" 
+					+ " in PopRxnsOutput_rxns.txt and PopRxnsOutput_spcs.txt");
+		} catch (FileNotFoundException e) {
+			System.err.println("File was not found!\n");
+		} catch(IOException e) {
+			System.err.println("Something wrong with ChemParser.readChemGraph");
+		}
+	}
+	
+	public static void initializeSystemProperties() {
+		String name= "RMG_database";
+		String workingDir = System.getenv("RMG");
+		System.setProperty("RMG.workingDirectory", workingDir);
+		System.setProperty("jing.chem.ChemGraph.forbiddenStructureFile",workingDir + "/databases/" + name + "/forbiddenStructure/forbiddenStructure.txt");
+		System.setProperty("jing.chem.ThermoGAGroupLibrary.pathName",	workingDir + "/databases/" + name + "/thermo");
+		System.setProperty("jing.rxn.ReactionTemplateLibrary.pathName",	workingDir + "/databases/" + name + "/kinetics");
+	};
 
 }
