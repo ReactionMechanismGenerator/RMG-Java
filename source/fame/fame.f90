baa840542ab0b33e0e606a864e974d71ab0c3f7a
Josh Allen <jwallen@mit.edu>
2010-04-01 10:49:47 -0400

    Updated primary FAME program to reflect recent changes.

--- source/fame/fame.f90
+++ source/fame/fame.f90
@@ -8,128 +8,79 @@
 
 program fame
 
-	use SpeciesModule
-	use IsomerModule
-	use ReactionModule
-	use NetworkModule
-	use InputModule
-	use StrongCollisionModule
-	use ReservoirStateModule
-	use ModelModule
-	use OutputModule
-	
-	implicit none
+    use NetworkModule
+    use IOModule
+    
+    implicit none
 
-	! The activated reaction network
-	type(Network) net
-	! K(t,p,i,j) = Phenomenological rate coefficients for transitions from species i to species j
-	real(8), dimension(:,:,:,:), allocatable 		:: 	K
-	! chebCoeff(t,p,i,j) = Coefficient matrix for product of Chebyshev polynomials phi_t(Tred) * phi_p(Pred) for reaction j --> i 
-	real(8), dimension(:,:,:,:), allocatable 		:: 	chebyshevCoefficients
-	! arrhenius(p,i,j) = Arrhenius kinetics matrix for logPInterpolate fitting model
-	type(ArrheniusKinetics), dimension(:,:,:), allocatable 		:: 	pDepArrhenius
-	! (T, P) = Current temperature and pressure
-	real(8)											::	T, P
-	! Indices
-	integer u, v, w, i, j, n, m
+    type(Network) net
+    real(8), dimension(:), allocatable :: Tlist, Plist, Elist
+    real(8) :: grainSize
+    integer :: numGrains
+    integer :: method, model
+    integer, dimension(1:10) :: modelOptions
+    real(8), dimension(:,:,:,:), allocatable :: K
+    real(8), dimension(:,:,:,:), allocatable :: chebyshev
+    type(ArrheniusKinetics), dimension(:,:,:), allocatable :: pDepArrhenius
+    integer :: nIsom, nReac, nProd, nGrains, nT, nP
 
-	! Read network information from stdin
-	call loadNetwork(net)
-	
-	! Determine energy range of calculation
-	call selectEnergyGrains(net)
-		
-	! Calculate density of states for each (unimolecular) well
-	do i = 1, size(net%isomers)
-		if (size(net%isomers(i)%species) == 1) then
-			call densityOfStates(net%isomers(i), net%species, net%E)
-		end if
-	end do
-	! Allocate memory for k(T, P) data
-	allocate( K( 1:size(net%Tlist), 1:size(net%Plist), 1:size(net%isomers), 1:size(net%isomers) ) )
-	
-	do u = 1, size(net%Tlist)
-		
-		T = net%Tlist(u)
-		
-		! Calculate the equilibrium (Boltzmann) distributions for each (unimolecular) well
-		do i = 1, size(net%isomers)
-			if (size(net%isomers(i)%species) == 1) then
-				call eqDist(net%isomers(i), net%E, T)
-			end if
-		end do
-		
-		! Calculate microcanonical rate coefficients at the current conditions
-		do w = 1, size(net%reactions)
-			call microRates(net%reactions(w), T, net%E, net%isomers, net%species)
-		end do
-		
-		do v = 1, size(net%Plist)
+    integer i
 
-			P = net%Plist(v)
+    ! Use unit 1 for logging; file will be called fame.log or fort.1
+    open(unit=1, file='fame.log')
 
-			! Calculate collision frequencies
-			do i = 1, size(net%isomers)
-				if (size(net%isomers(i)%species) == 1) then
-					net%isomers(i)%collFreq = collisionFrequency(T, P, net%bathGas, &
-						net%species(net%isomers(i)%species(1))%general)
-				end if
-			end do
-			
-			! Determine phenomenological rate coefficients
-			if (net%method == 1) then
-				call ssmscRates(net, T, P, K(u,v,:,:))
-			elseif (net%method == 2) then
-				call ssrsRates(net, T, P, K(u,v,:,:))
-			end if
-			
-			! Check for validity of phenomenological rate coefficients
-			do i = 1, size(net%isomers)
-				do j = 1, size(net%isomers)
-					if (i /= j) then
-						if (K(u,v,i,j) <= 0) then
-							write (0, fmt='(A)') 'One ore more rate coefficients not properly estimated.'
-							stop
-						end if
-					end if
-				end do
-			end do
-			
-			! Convert units of bimolecular k(T, P) to cm^3/mol*s
-			do j = 1, size(net%isomers)
-				if (size(net%isomers(j)%species) > 1) then
-					do i = 1, size(net%isomers)
-						K(u,v,i,j) = K(u,v,i,j) * 1.0e6
-					end do
-				end if
-			end do
-			
-			
-		end do
-		
-	end do
-	
-	! Fit phenomenological rate coefficients to selected model
-	if (net%model == 1) then
-		allocate( chebyshevCoefficients(net%numChebT, net%numChebP, size(net%isomers), size(net%isomers)) )
-	elseif (net%model == 2) then
-		allocate( pDepArrhenius(size(net%Plist), size(net%isomers), size(net%isomers)) )
-	end if
-	do i = 1, size(net%isomers)
-		do j = 1, size(net%isomers)
-			if (i /= j) then
-				if (net%model == 1) then
-					call fitChebyshevModel(K(:,:,i,j), net%Tlist, net%Plist, net%numChebT, net%numChebP, chebyshevCoefficients(:,:,i,j))
-				elseif (net%model == 2) then
-					call fitPDepArrheniusModel(K(:,:,i,j), net%Tlist, net%Plist, pDepArrhenius(:,i,j))
-				end if
-			end if
-		end do
-	end do
-	
-	! Write results to stdout
-	call saveNetwork(net, K, chebyshevCoefficients, pDepArrhenius)
-	
+    ! Log header
+
+    ! Read network information (from stdin)
+    write (1,fmt='(A)') 'Reading network information...'
+    call readInput(net, Tlist, Plist, grainSize, numGrains, method, model, modelOptions)
+    nIsom = 0
+    nReac = 0
+    nProd = 0
+    do i = 1, size(net%isomers)
+        if (size(net%isomers(i)%species) > 1) then
+            nReac = nReac + 1
+        else
+            nIsom = nIsom + 1
+        end if
+    end do
+    nT = size(Tlist)
+    nP = size(Plist)
+
+    ! Shift network such that lowest-energy isomer has a ground state of 0.0
+    write (1,fmt='(A)') 'Zeroing lowest energy isomer...'
+    call network_shiftToZeroEnergy(net)
+
+    ! Determine energy grains
+    write (1,fmt='(A)') 'Determining energy grains...'
+    call network_determineEnergyGrains(net, nIsom, grainSize, numGrains, maxval(Tlist), Elist)
+    nGrains = size(Elist)
+    
+    ! Calculate density of states for all isomers in network
+    write (1,fmt='(A)') 'Calculating densities of states...'
+    do i = 1, nIsom
+        call isomer_getDensityOfStates(net, net%isomers(i), Elist, nGrains)
+    end do
+
+    ! Determine phenomenological rate coefficients
+    write (1,fmt='(A)') 'Calculating phenomenological rate coefficients...'
+    allocate( K( 1:nT, 1:nP, 1:nIsom+nReac+nProd, 1:nIsom+nReac+nProd) )
+    call network_calculateRateCoefficients(net, nIsom, nReac, nProd, &
+        Elist, nGrains, Tlist, nT, Plist, nP, method, K)
+
+    ! Fit interpolation model
+    !write (1,fmt='(A)') 'Fitting interpolation model...'
+    !call network_fitInterpolationModel(net, Tlist, nT, Plist, nP, K, nIsom+nReac+nProd, model, modelOptions)
+
+    ! Write results (to stdout)
+    write (1,fmt='(A)') 'Writing results...'
+    call writeOutput(net, nIsom+nReac+nProd, Tlist, nT, Plist, nP, Elist, nGrains, &
+        method, K, model, modelOptions, chebyshev, pDepArrhenius)
+
+    ! Close log file
+    close(1)
+
+    deallocate(K)
+
 end program
 
-! ==============================================================================
