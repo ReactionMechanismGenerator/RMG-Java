678b29f55bdfa2d0cf912ce5819f38127d6e88ed
Gregory Magoon <gmagoon@mit.edu>
2010-05-11 20:39:01 -0400

    a fix that should satisfy Pharos/Linux and Windows (with env. vars. with spaces)
    apparently Linux doesn't like arguments being passed with quotes via Java (though it seems to work OK on command line(?))

--- source/RMG/jing/chem/QMTP.java
+++ source/RMG/jing/chem/QMTP.java
@@ -372,13 +372,24 @@ public class QMTP implements GeneralGAPP {
         String name = twoDmolFile.getName();
         try{   
             File runningdir=new File(directory);
-            String command = "python \""+System.getProperty("RMG.workingDirectory")+"/scripts/distGeomScriptMolLowestEnergyConf.py\" ";
-            String twoDmolpath=twoDmolFile.getPath();
-            command=command.concat("\""+twoDmolpath+"\" ");
-            command=command.concat("\""+name+".mol\" ");//this is the target file name; use the same name as the twoDmolFile (but it will be in he 3Dmolfiles folder
-            command=command.concat(numConfAttempts + " ");
-	    command=command.concat("\"" + System.getenv("RDBASE")+"\"");//pass the $RDBASE environment variable to the script so it can use the approprate directory when importing rdkit
-            Process pythonProc = Runtime.getRuntime().exec(command, null, runningdir);
+	    String command="";
+	    if (System.getProperty("os.name").toLowerCase().contains("windows")){//special windows case where paths can have spaces and are allowed to be surrounded by quotes
+		command = "python \""+System.getProperty("RMG.workingDirectory")+"/scripts/distGeomScriptMolLowestEnergyConf.py\" ";
+		String twoDmolpath=twoDmolFile.getPath();
+		command=command.concat("\""+twoDmolpath+"\" ");
+		command=command.concat("\""+name+".mol\" ");//this is the target file name; use the same name as the twoDmolFile (but it will be in he 3Dmolfiles folder
+		command=command.concat(numConfAttempts + " ");
+		command=command.concat("\"" + System.getenv("RDBASE")+"\"");//pass the $RDBASE environment variable to the script so it can use the approprate directory when importing rdkit
+	    }    
+	    else{//non-Windows case
+		command = "python "+System.getProperty("RMG.workingDirectory")+"/scripts/distGeomScriptMolLowestEnergyConf.py ";
+		String twoDmolpath=twoDmolFile.getPath();
+		command=command.concat(""+twoDmolpath+" ");
+		command=command.concat(name+".mol ");//this is the target file name; use the same name as the twoDmolFile (but it will be in he 3Dmolfiles folder
+		command=command.concat(numConfAttempts + " ");
+		command=command.concat(System.getenv("RDBASE"));//pass the $RDBASE environment variable to the script so it can use the approprate directory when importing rdkit
+	    }
+	    Process pythonProc = Runtime.getRuntime().exec(command, null, runningdir);
             String killmsg= "Python process for "+twoDmolFile.getName()+" did not complete within 120 seconds, and the process was killed. File was probably not written.";//message to print if the process times out
             Thread timeoutThread = new TimeoutKill(pythonProc, killmsg, 120000L); //create a timeout thread to handle cases where the UFF optimization get's locked up (cf. Ch. 16 of "Ivor Horton's Beginning Java 2: JDK 5 Edition"); once we use the updated version of RDKit, we should be able to get rid of this
             timeoutThread.start();//start the thread
@@ -955,9 +966,16 @@ public class QMTP implements GeneralGAPP {
 //        return result;
         
         String command = "python "+ System.getProperty("RMG.workingDirectory")+"/scripts/GaussianPM3ParsingScript.py ";
-        String logfilepath="\""+directory+"/"+name+".log\"";
-        command=command.concat(logfilepath);
-	command=command.concat(" "+ System.getenv("RMG")+"/source");//this will pass $RMG/source to the script (in order to get the appropriate path for importing
+        if (System.getProperty("os.name").toLowerCase().contains("windows")){//special windows case where paths can have spaces and are allowed to be surrounded by quotes
+	    String logfilepath="\""+directory+"/"+name+".log\"";
+	    command=command.concat(logfilepath);
+	    command=command.concat(" \""+ System.getenv("RMG")+"/source\"");//this will pass $RMG/source to the script (in order to get the appropriate path for importing
+	}
+	else{//non-Windows case
+	    String logfilepath=directory+"/"+name+".log";
+	    command=command.concat(logfilepath);
+	    command=command.concat(" "+ System.getenv("RMG")+"/source");//this will pass $RMG/source to the script (in order to get the appropriate path for importing
+	}
 	System.out.println(command);//debugging line
         ThermoData result = getPM3ThermoDataUsingCCLib(name, directory, p_chemGraph, command);
         System.out.println("Thermo for " + name + ": "+ result.toString());//print result, at least for debugging purposes
@@ -967,9 +985,16 @@ public class QMTP implements GeneralGAPP {
     //parse the results using cclib and return a ThermoData object; name and directory indicate the location of the MOPAC .out file
     public ThermoData parseMopacPM3(String name, String directory, ChemGraph p_chemGraph){
         String command = "python "+System.getProperty("RMG.workingDirectory")+"/scripts/MopacPM3ParsingScript.py ";
-        String logfilepath="\""+directory+"/"+name+".out\"";
-        command=command.concat(logfilepath);
-	command=command.concat(" "+ System.getenv("RMG")+"/source");//this will pass $RMG/source to the script (in order to get the appropriate path for importing
+        if (System.getProperty("os.name").toLowerCase().contains("windows")){//special windows case where paths can have spaces and are allowed to be surrounded by quotes
+	    String logfilepath="\""+directory+"/"+name+".out\"";
+	    command=command.concat(logfilepath);
+	    command=command.concat(" \""+ System.getenv("RMG")+"/source\"");//this will pass $RMG/source to the script (in order to get the appropriate path for importing
+	}
+	else{//non-Windows case
+	    String logfilepath=directory+"/"+name+".out";
+	    command=command.concat(logfilepath);
+	    command=command.concat(" "+ System.getenv("RMG")+"/source");//this will pass $RMG/source to the script (in order to get the appropriate path for importing
+	}
         ThermoData result = getPM3ThermoDataUsingCCLib(name, directory, p_chemGraph, command);
         System.out.println("Thermo for " + name + ": "+ result.toString());//print result, at least for debugging purposes
         return result;
