0cdea069afe789bd96ef3102f7b4daf8ea941fd4
Gregory Magoon <gmagoon@mit.edu>
2010-05-12 22:12:47 -0400

    fixed G03 execution to allow running directories with spaces in Windows
    if PATH includes the G03 directory, G03 execution no longer requires a specific running directory and we can use relative paths for input and output files

--- source/RMG/jing/chem/QMTP.java
+++ source/RMG/jing/chem/QMTP.java
@@ -702,17 +702,11 @@ public class QMTP implements GeneralGAPP {
         int flag = 0;
         int successFlag=0;
         try{ 
-            File runningdir=new File(System.getenv("G03DIR"));//tests suggest that we need to run from this directory or else l1.exe cannon be found
-            String command = System.getenv("G03DIR")+"/g03 ";
-	    if (System.getProperty("os.name").toLowerCase().contains("windows")){//special windows case where paths can have spaces and are allowed to be surrounded by quotes
-		command=command.concat("\""+directory+"/"+name+".gjf\" ");//specify the input file; space is important
-		command=command.concat("\""+directory+"/"+name+".log\"");//specify the output file
-	    }
-	    else{
-		command=command.concat(directory+"/"+name+".gjf ");//specify the input file; space is important
-		command=command.concat(directory+"/"+name+".log");//specify the output file
-	    }
-            Process gaussianProc = Runtime.getRuntime().exec(command, null, runningdir);
+            String command = "g03 ";
+            command=command.concat(qmfolder+"/"+name+".gjf ");//specify the input file; space is important
+	    command=command.concat(qmfolder+"/"+name+".log");//specify the output file
+	    Process gaussianProc = Runtime.getRuntime().exec(command);
+
             //check for errors and display the error if there is one
             InputStream is = gaussianProc.getErrorStream();
             InputStreamReader isr = new InputStreamReader(is);
