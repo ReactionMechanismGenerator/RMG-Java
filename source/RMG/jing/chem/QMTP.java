83286d0bddf813bc5c4b755c4e71a0b141bb7a06
Gregory Magoon <gmagoon@mit.edu>
2010-06-03 20:07:44 -0400

    draft code for successfulMM4ResultExistsQ
    I need to make sure that input file includes full InChI in first line after first 80 characters

--- source/RMG/jing/chem/QMTP.java
+++ source/RMG/jing/chem/QMTP.java
@@ -1681,6 +1681,114 @@ public class QMTP implements GeneralGAPP {
         //we could print a line here for cases where the file doesn't exist, but this would probably be too verbose
         return false;
     }
+
+    //returns true if an MM4 output file for the given name and directory (.mm4out suffix) exists and indicates successful completion (same criteria as used after calculation runs); terminates if the InChI doesn't match the InChI in the file or if there is no InChI in the file; returns false otherwise
+    public boolean successfulMM4ResultExistsQ(String name, String directory, String InChIaug){
+        //part of the code is taken from analogous code for MOPAC (first ~half) and Gaussian (second ~half)
+        //look in the output file to check for the successful termination of the calculation (assumed to be successful if "description of vibrations appears)
+        File file = new File(directory+"/"+name+".mm4out");
+        if(file.exists()){//if the file exists, do further checks; otherwise, we will skip to final statement and return false
+            int failureFlag=1;//flag (1 or 0) indicating whether the MM4 job failed
+            //int failureOverrideFlag=0;//flag (1 or 0) to override success as measured by failureFlag
+            int InChIMatch=0;//flag (1 or 0) indicating whether the InChI in the file matches InChIaug; this can only be 1 if InChIFound is also 1;
+            int InChIFound=0;//flag (1 or 0) indicating whether an InChI was found in the log file
+            int InChIPartialMatch=0;//flag (1 or 0) indicating whether the InChI in the log file is a substring of the InChI in RMG's memory
+            String logFileInChI="";
+            try{
+                FileReader in = new FileReader(file);
+                BufferedReader reader = new BufferedReader(in);
+                String line=reader.readLine();
+                while(line!=null){
+                    String trimLine= line.trim();
+                    if (trimLine.equals("STATISTICAL THERMODYNAMICS ANALYSIS")){
+                       failureFlag = 0;
+                    }
+                 //   else if (trimLine.endsWith("IMAGINARY FREQUENCIES")){
+                 //     //  System.out.println("*****Imaginary freqencies found:");
+                 //       failureOverrideFlag=1;
+                 //   }
+                    else if(trimLine.startsWith("InChI=")){
+                        logFileInChI = line.trim();//output files should take up to about 60 (?) characters of the name in the input file
+                        InChIFound=1;
+                        if(logFileInChI.equals(InChIaug)) InChIMatch=1;
+                        else if(InChIaug.startsWith(logFileInChI)) InChIPartialMatch=1;
+                    }
+                    line=reader.readLine();
+                }
+            }
+            catch(Exception e){
+                String err = "Error in reading preexisting MM4 output file \n";
+                err += e.toString();
+                e.printStackTrace();
+                System.exit(0);
+            }
+           // if(failureOverrideFlag==1) failureFlag=1; //job will be considered a failure if there are imaginary frequencies or if job terminates to to excess time/cycles
+            //if the failure flag is still 0, the process should have been successful
+            if (failureFlag==0&&InChIMatch==1){
+                System.out.println("Pre-existing successful MM4 result for " + name + " ("+InChIaug+") has been found. This log file will be used.");
+                return true;
+            }
+            else if (InChIFound==1 && InChIMatch == 0){//InChIs do not match (most likely due to limited name length mirrored in log file (79 characters), but possibly due to a collision)
+                if(InChIPartialMatch == 1){//case where the InChI in memory begins with the InChI in the log file; we will continue and check the input file, printing a warning if there is no match
+                    File inputFile = new File(directory+"/"+name+".mm4");
+                    if(inputFile.exists()){//read the MM4 inputFile
+                        String inputFileInChI="";
+                        try{
+                            FileReader inI = new FileReader(inputFile);
+                            BufferedReader readerI = new BufferedReader(inI);
+                            String lineI=readerI.readLine();
+			    //InChI should be repeated after in the first line of the input file
+                            inputFileInChI = lineI.trim().substring(80);//extract the string starting with character 81
+                        }
+                        catch(Exception e){
+                            String err = "Error in reading preexisting MM4 .mm4 file \n";
+                            err += e.toString();
+                            e.printStackTrace();
+                            System.exit(0);
+                        }
+                        if(inputFileInChI.equals(InChIaug)){
+                            if(failureFlag==0){
+                                System.out.println("Pre-existing successful MM4 result for " + name + " ("+InChIaug+") has been found. This log file will be used. *Note that input file was read to confirm lack of InChIKey collision (InChI probably more than ~60 characters)");
+                                return true;
+                            }
+                            else{//otherwise, failureFlag==1
+                                System.out.println("Pre-existing MM4 result for " + name + " ("+InChIaug+") has been found, but the result was apparently unsuccessful. The file will be overwritten with a new calculation. *Note that input file was read to confirm lack of InChIKey collision (InChI probably more than ~60 characters)");
+                                return false;
+                            }
+                        }
+                        else{
+                            if(inputFileInChI.equals("")){//InChI was not found in input file
+                                System.out.println("*****Warning: potential InChIKey collision: InChIKey(augmented) = " + name + " RMG Augmented InChI = "+ InChIaug + " Log file Augmented InChI = "+logFileInChI + " . InChI could not be found in the MM4 input file. You should manually check that the log file contains the intended species.");
+                                return true;
+                            }
+                            else{//InChI was found but doesn't match
+                                System.out.println("Congratulations! You appear to have discovered the first recorded instance of an InChIKey collision: InChIKey(augmented) = " + name + " RMG Augmented InChI = "+ InChIaug + " MM4 input file Augmented InChI = "+inputFileInChI);
+                                System.exit(0);
+                            }
+                        }
+                    }
+                    else{
+                        System.out.println("*****Warning: potential InChIKey collision: InChIKey(augmented) = " + name + " RMG Augmented InChI = "+ InChIaug + " Log file Augmented InChI = "+logFileInChI + " . MM4 input file could not be found to check full InChI. You should manually check that the log file contains the intended species.");
+                        return true;
+                    }
+                }
+                else{
+                    System.out.println("Congratulations! You appear to have discovered the first recorded instance of an InChIKey collision: InChIKey(augmented) = " + name + " RMG Augmented InChI = "+ InChIaug + " Log file Augmented InChI = "+logFileInChI);
+                    System.exit(0);
+                }
+            }
+            else if (InChIFound==0){
+                System.out.println("An InChI was not found in file: " +name+".mm4out");
+                System.exit(0);
+            }
+            else if (failureFlag==1){//note these should cover all possible results for this block, and if the file.exists block is entered, it should return from within the block and should not reach the return statement below
+                System.out.println("Pre-existing MM4 result for " + name + " ("+InChIaug+") has been found, but the result was apparently unsuccessful. The file will be overwritten with a new calculation.");
+                return false;
+            }
+        }
+        //we could print a line here for cases where the file doesn't exist, but this would probably be too verbose
+        return false;
+    }
     
 //    //checks the MOPAC file for negative frequencies
 //    public boolean MopacFileContainsNegativeFreqsQ(String name, String directory){
