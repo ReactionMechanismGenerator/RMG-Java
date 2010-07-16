4d6afca38caae1359f71af63eb124b4e57a9ce66
Amrit Jalan <amrit@mit.edu>
2010-07-16 14:15:39 -0400

    Use Mintz model to estimate solution phase enthalpy and entropy.
    The previous approach from Rob Ashcraft (based on Pierotti's Scaled Particle
    Theroy) has been replaced by the Mintz model. Current solvent parameters for
    the free energy and enthalpy are those for decane and n-alkanes respectively.

--- source/RMG/jing/chem/GATP_Solvation.java
+++ source/RMG/jing/chem/GATP_Solvation.java
@@ -44,23 +44,30 @@ public class GATP_Solvation implements GeneralSolvationGAPP {
     }
 	
 	public ThermoData generateSolvThermoData(ChemGraph p_chemGraph) {
-		double r_solute=p_chemGraph.getRadius();               // Returns VdW radius in meter
-		double r_solvent; r_solvent=3.498e-10;// 3.311;        // Manually assigned solvent radius [=] meter Calculated using Connolly solvent excluded volume from Chem3dPro
-		double r_cavity=r_solute+r_solvent;                    // Cavity radius [=] meter
-		double rho; rho=3.09e27;  //0.00381;                   // number density of solvent [=] molecules/m^3   Value here is for decane using density =0.73 g/cm3
-		double parameter_y=4.1887902*rho*Math.pow(r_solvent, 3);  // Parameter y from Ashcraft Thesis Refer pg no. 60. (4/3)*pi*rho*r^3
-		double parameter_ymod=parameter_y/(1-parameter_y);     // parameter_ymod= y/(1-y) Defined for convenience
-		double R=8.314;                                        // Gas constant units J/mol K
-		double T=298;                                          // Standard state temperature
+        
+        //double r_solute=p_chemGraph.getRadius();               // Returns VdW radius in meter
+		//double r_solvent; r_solvent=3.498e-10;// 3.311;        // Manually assigned solvent radius [=] meter Calculated using Connolly solvent excluded volume from Chem3dPro
+		//double r_cavity=r_solute+r_solvent;                    // Cavity radius [=] meter
+		//double rho; rho=3.09e27;  //0.00381;                   // number density of solvent [=] molecules/m^3   Value here is for decane using density =0.73 g/cm3
+		//double parameter_y=4.1887902*rho*Math.pow(r_solvent, 3);  // Parameter y from Ashcraft Thesis Refer pg no. 60. (4/3)*pi*rho*r^3
+		//double parameter_ymod=parameter_y/(1-parameter_y);     // parameter_ymod= y/(1-y) Defined for convenience
 		
 		// Definitions of K0, K1 and K2 correspond to those for K0', K1' and K2' respectively from Ashcraft's Thesis (-d/dT of K0,K1,K2)
-		double K0= -R*(-Math.log(1-parameter_y)+(4.5*parameter_ymod*parameter_ymod));
-		double K1= (R*0.5/r_solvent)*((6*parameter_ymod)+(18*parameter_ymod*parameter_ymod));
-		double K2= -(R*0.25/(r_solvent*r_solvent))*((12*parameter_ymod)+(18*parameter_ymod*parameter_ymod));
-		
+//		double K0= -R*(-Math.log(1-parameter_y)+(4.5*parameter_ymod*parameter_ymod));
+//		double K1= (R*0.5/r_solvent)*((6*parameter_ymod)+(18*parameter_ymod*parameter_ymod));
+//		double K2= -(R*0.25/(r_solvent*r_solvent))*((12*parameter_ymod)+(18*parameter_ymod*parameter_ymod));
+//
 		// Basic definition of entropy change of solvation from Ashcfrat's Thesis
-		double deltaS0;
-		deltaS0=K0+(K1*r_cavity)+(K2*r_cavity*r_cavity);
+//		double deltaS0;
+//		deltaS0=K0+(K1*r_cavity)+(K2*r_cavity*r_cavity);
+
+
+    /* AJ 16JULY2010
+     * The Pierotti method has now been replaced with the method of MIntz et al. based on some recent comparisons between the 2 methods
+     */
+        
+        double R=8.314;                                        // Gas constant units J/mol K
+		double T=298;                                          // Standard state temperature
 		
 		// Generation of Abraham Solute Parameters
 		AbramData result_Abraham= new AbramData();
@@ -72,28 +79,37 @@ public class GATP_Solvation implements GeneralSolvationGAPP {
 		double E=result_Abraham.E;
 		double L=result_Abraham.L;
 		double A=result_Abraham.A;
+        double V=result_Abraham.V;
 		
-        //Manually specified solvent descriptors 
+        //Manually specified solvent descriptors for free energy calculation
 		// (constants here are for dry decane, from from M.H. Abraham et al. / J. Chromatogr. A 1037 (2004) 29–47 )
-        double c = 0.156;
-        double s = 0;
-        double b = 0; 
-        double e = -0.143;
-        double l = 0.989;
-        double a = 0;
+        double c_g = 0.156;
+        double s_g = 0;
+        double b_g = 0;
+        double e_g = -0.143;
+        double l_g = 0.989;
+        double a_g = 0;
 		
-		// Dry octan-1-ol, from M.H. Abraham et al. / J. Chromatogr. A 1037 (2004) 29–47 
-		// c=-0.120; e=-0.203; s=0.560; a=3.560; b=0.702; l=0.939
-		
-        double logK = c + s*S + b*B + e*E + l*L + a*A;    // Implementation of Abraham Model
-        double deltaG0_decane = -8.314*298*logK;
-		//       System.out.println("The free energy of solvation in decane at 298K w/o reference state corrections  = " + deltaG0_decane +" J/mol for " );
-		
-        // Calculation of enthalpy change of solvation using the data obtained above
-        double deltaH0=deltaG0_decane+(T*deltaS0);
-        deltaS0=deltaS0/4.18;   //unit conversion from J/mol to cal/mol
-        deltaH0=deltaH0/4180;   //unit conversion from J/mol to kcal/mol
-		
+
+        double logK = c_g + s_g*S + b_g*B + e_g*E + l_g*L + a_g*A;    // Implementation of Abraham Model for calculation of partition coefficient
+        double deltaG0 = -8.314*298*logK;
+		// System.out.println("The free energy of solvation in decane at 298K w/o reference state corrections  = " + deltaG0_decane +" J/mol for " );
+
+    /* AJ 16JULY 2010 (Mintz method for calculation of solution phase enthalpy
+     */
+
+        // Manually specified solvent descriptors for enthalpy calculations
+		// (constants here are for alkanes, from from C. Mintz et al. / J. Chromatogr. A 1037 (2004) 29–47 )
+        double c_h = -6.708;
+        double s_h = 0;
+        double b_h = 0;
+        double e_h = 2.999;
+        double l_h = -9.279;
+        double a_h = 0;
+
+        double deltaH0 = c_g + s_g*S + b_g*B + e_g*E + l_g*L + a_g*A;    // Implementation of Abraham Model for calculation of partition coefficient
+        double deltaS0 = (deltaH0-deltaG0)/T;
+
 		// Generation of Gas Phase data to add to the solution phase quantities
 		ThermoData solvationCorrection = new ThermoData(deltaH0, deltaS0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,"Solvation correction");
 		
