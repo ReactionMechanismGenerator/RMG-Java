aeaa07ce345de237f5e5f561c1c145a8f0161943
Richard West <rwest@mit.edu>
2010-07-20 23:50:00 -0400

    Save fame input to fame/#_input.txt before calling fame.exe
    The java code saves the input it's about to send to fame.
    These are numbered by the network number so will be over-written
    each time the network is expanded and resolved.

--- source/RMG/jing/rxn/FastMasterEqn.java
+++ source/RMG/jing/rxn/FastMasterEqn.java
@@ -289,14 +289,14 @@ public class FastMasterEqn implements PDepKineticsEstimator {
         StringBuffer output = new StringBuffer(2620); // of several I tested, the smallest FAME output was 2609 characters.
 
 		// DEBUG only: Write input file
-		/*try {
+		try {
 			FileWriter fw = new FileWriter(new File("fame/" + Integer.toString(pdn.getID()) + "_input.txt"));
 			fw.write(input);
 			fw.close();
 		} catch (IOException ex) {
 			System.out.println("Unable to write FAME input file.");
 			System.exit(0);
-		}*/
+		}//*/
 
 		// Touch FAME output file
 		//touchOutputFile(); //no longer needed with standard input/output
