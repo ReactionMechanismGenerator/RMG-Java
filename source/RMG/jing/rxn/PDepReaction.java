ReactionMechanismGenerator/RMG-Java

commit f9518240b1f66d90690eddf4a3e142b33a5b1f36

Chemkin reaction strings are padded to 52 characters long.
This makes Chemkin and restart files a lot prettier to look at.

source/RMG/jing/rxn/PDepReaction.java
@@ -518,7 +518,8 @@ public class PDepReaction extends Reaction {
 			String result = getStructure().toChemkinString(true).toString();
 			if (PDepRateConstant.getMode() == PDepRateConstant.Mode.CHEBYSHEV)
 				result = formPDepSign(result);
-			result += '\t' + "1.0E0 0.0 0.0" ;
+			result = String.format("%-52s",result);
+			result += "\t1.0E0 0.0 0.0" ;
 			result += "\t!" + getComments().toString()  + '\n';
 			if (PDepRateConstant.getMode() == PDepRateConstant.Mode.CHEBYSHEV)
 				result += pDepRate.getChebyshev().toChemkinString() + '\n';
@@ -545,7 +546,8 @@ public class PDepReaction extends Reaction {
 			String result = getStructure().toRestartString(true).toString();
 			if (PDepRateConstant.getMode() == PDepRateConstant.Mode.CHEBYSHEV)
 				result = formPDepSign(result);
-			result += '\t' + "1.0E0 0.0 0.0" ;
+			result = String.format("%-52s",result);
+			result += "\t1.0E0 0.0 0.0" ;
 			result += "\t!" + getComments().toString() +
 				"\tdeltaHrxn(T=298K) = " + 
 				calculateHrxn(new Temperature(298.0,"K")) + " kcal/mol\n";
@@ -575,17 +577,21 @@ public class PDepReaction extends Reaction {
 	@Override
 	public String toChemkinString(Temperature t, Pressure p) {
         if (pDepRate != null) {
-
 			String result = getStructure().toChemkinString(true).toString();
-			if (PDepRateConstant.getMode() == PDepRateConstant.Mode.CHEBYSHEV)
-				result = formPDepSign(result);
+
 			if (PDepRateConstant.getMode() == PDepRateConstant.Mode.RATE) {
-				result = formPDepSign(result) + "\t" + calculateRate(t,p) + " 0.0 0.0";
-				result += "\t!" + getComments().toString()  + '\n';
+				result = String.format(result = formPDepSign(result));
+				result += String.format("\t%.3e 0.0 0.0",calculateRate(t,p));
+				result += "\t!" + getComments().toString() + '\n';
 				return result;
 			}
-			result += '\t' + "1.0E0 0.0 0.0";
-			result += "\t!" + getComments().toString()  + '\n';
+
+			if (PDepRateConstant.getMode() == PDepRateConstant.Mode.CHEBYSHEV)
+				result = formPDepSign(result);
+			result = String.format("%-52s",result);
+
+			result += "\t1.0E0 0.0 0.0";
+			result += "\t!" + getComments().toString() + '\n';
 
 			if (PDepRateConstant.getMode() == PDepRateConstant.Mode.CHEBYSHEV)
 				result += pDepRate.getChebyshev().toChemkinString() + '\n';

source/RMG/jing/rxn/Reaction.java
@@ -1336,7 +1336,7 @@ public class Reaction {
 	  if (ChemkinString != null)
 		  return ChemkinString;
 	  	StringBuilder result = new StringBuilder();
-      	StringBuilder strucString = getStructure().toChemkinString(hasReverseReaction());
+      	String strucString = String.format("%-52s",getStructure().toChemkinString(hasReverseReaction()));
 		Temperature stdtemp = new Temperature(298,"K");
 		double Hrxn = calculateHrxn(stdtemp);
 		Kinetics[] allKinetics = getKinetics();
@@ -1378,7 +1378,7 @@ public class Reaction {
 		 * 		when writing the Restart files, to account for this bug.
 		 */
 		
-		String result = getStructure().toRestartString(hasReverseReaction()).toString(); //+ " "+getStructure().direction + " "+getStructure().redundancy;
+		String result = String.format("%-52s",getStructure().toRestartString(hasReverseReaction())); //+ " "+getStructure().direction + " "+getStructure().redundancy;
 		// MRH 18Jan2010: Restart files do not look for direction/redundancy
 		/*
 		 * MRH 14Feb2010: Handle reactions with multiple kinetics

source/RMG/jing/rxn/ThirdBodyReaction.java
@@ -215,6 +215,7 @@ public class ThirdBodyReaction extends Reaction {
       //#[ operation toChemkinString() 
       StringBuilder s = getStructure().toChemkinString(true);
       s = formPDepSign(s);
+	  s = new StringBuilder(String.format("%-52s",s)); // pad it to 52 characters
       for (int i=0; i<getKinetics().length; i++) {
     	  s.append("\t" + getKinetics()[i].toChemkinString(calculateHrxn(p_temperature),p_temperature, true) + '\n');
       }
@@ -249,6 +250,7 @@ public class ThirdBodyReaction extends Reaction {
   public String toRestartString(Temperature t) {
       StringBuilder s = getStructure().toRestartString(true);
       s = formPDepSignForRestart(s);
+	  s = new StringBuilder(String.format("%-52s",s)); // pad it to 52 characters
       for (int i=0; i<getKinetics().length; i++) {
     	  s.append("\t" + getKinetics()[i].toChemkinString(calculateHrxn(t),t,false) + " 0.0 0.0 0.0\n");
       }

commit 912e7d2c51fb53f77355e9019ceba44bd9dacc19

Fix bug and improve reporting of RATE-formatted P-dep rates.
One mistake was introduced in f9518240b1f66d90690eddf4a3e142b33a5b1f36.

There was an additional mistake (I think) in that it was adding (+m), but
these rate coefficients are, as far as Chemkin is concerned, just a plain
old function of temperature. This mistake was older.

I have also added a comment about the conditions at which the rate was evaluated.

source/RMG/jing/rxn/PDepReaction.java
@@ -580,9 +580,10 @@ public class PDepReaction extends Reaction {
 			String result = getStructure().toChemkinString(true).toString();
 
 			if (PDepRateConstant.getMode() == PDepRateConstant.Mode.RATE) {
-				result = String.format(result = formPDepSign(result));
-				result += String.format("\t%.3e 0.0 0.0",calculateRate(t,p));
-				result += "\t!" + getComments().toString() + '\n';
+				result = String.format("%-52s",result);
+				result += String.format("\t%.3e   0.0   0.0 ",calculateRate(t,p));
+				result += "\t!" + getComments().toString();
+				result += String.format(" Evaluated at T=%s K P=%s bar\n",t.getK(),p.getBar());
 				return result;
 			}
 