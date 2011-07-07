ReactionMechanismGenerator/RMG-Java

commit 7339cc176cc7bbc671f4dec65b2f161d351ea270

If unnamed species is in a Primary Thermo Library, use the name from that library.
If a species is not named in the input file, but is named in a primary thermo library,
and that name is not an ugly PrIMe ID ("s01234567") then use the name from
the primary thermo library instead of the chemical formula.

This will change the apparent output of some RMG jobs when you change the
primary thermo libraries. The inconsistency is a minor inconvenience, but I
think having descriptive names instead of just the atom counts (molecular formula)
is an improvement...?

source/RMG/jing/chem/Species.java
@@ -875,6 +875,17 @@ public class Species {
 			int id= ++TOTAL_NUMBER;
 			
         	spe = new Species(id,name,p_chemGraph);
+			
+			// If p_name was not specified, then try to get the name from eth primarythermolibrary.
+			if ((p_name == null || p_name.length()==0) && p_chemGraph.fromprimarythermolibrary){
+				name = spe.getThermoData().getName();
+				if (name.matches("s\\d{8}")){
+					// it's an ugly PrIMe ID! Ignore it.
+				} else{
+					// use the name from the primary thermo library!
+					spe.setName(spe.getThermoData().getName());
+				}
+			}
         	//spe.ID =
         	dictionary.putSpecies(spe, true);
         	

commit 8c5f991e6876de001ff11829ceb9894d11c80014

When changing a species name, update the Chemkin thermo NASA polynomials.
This fixes a bug introduced in 7339cc176cc7bbc671f4dec65b2f161d351ea270
in which we rename species using their names from primary thermo libraries, 
but the chemkin file ended up with a thermo section containing the old name.
Thanks to @connie for reporting the bug, and apologies from me for introducing.

source/RMG/jing/chem/Species.java
@@ -881,6 +881,9 @@ public class Species {
 				} else{
 					// use the name from the primary thermo library!
 					spe.setName(spe.getThermoData().getName());
+					// regenerate the chemkin-formatted NASA polynomials (small waste of time) because 
+					// these contain the species name, which we just changed.
+					spe.generateNASAThermoData();
 				}
 			}
         	//spe.ID =