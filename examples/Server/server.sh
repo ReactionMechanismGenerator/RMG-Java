45313832bc61c28eb48bcce78d1aac1710ae8055
Richard West <rwest@mit.edu>
2011-08-11 11:13:51 -0400

    PopulateReactionsServer shell script no longer tees to RMG.log
    Now that the class initializes the logger module, stuff is already saved
    to the RMG.log file, so this is redundant and confusing.

--- examples/Server/server.sh
+++ examples/Server/server.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 echo "Running PopulateReactionsServer..."
-java -Xmx500m -classpath $RMG/bin/RMG.jar PopulateReactionsServer input.txt 2>&1 | tee RMG.log
-#java -Xmx500m -classpath $RMG/build/RMG/ PopulateReactionsServer input.txt 2>&1 | tee RMG.log
+java -Xmx500m -classpath $RMG/bin/RMG.jar PopulateReactionsServer input.txt 2>&1
+#java -Xmx500m -classpath $RMG/build/RMG/ PopulateReactionsServer input.txt 2>&1
 echo "PopulateReactions job completed. Results saved in RMG.log"
