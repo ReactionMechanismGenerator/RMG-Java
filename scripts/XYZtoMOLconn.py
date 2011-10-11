a345b3878a9348d41f9212cba700217211ff92b8
Gregory Magoon <gmagoon@mit.edu>
2011-10-11 15:16:09 -0400

    added a backup connectivity check approach; also refactored some parts of QMTP
    original approach with OpenBabel may suggest a mismatch when there isn't one due to either OpenBabel segmentation fault (e.g. http://sourceforge.net/tracker/?func=detail&aid=3417992&group_id=40728&atid=428740) producing empty InChI or tolerances that are too tight (e.g. InChI=1/C10H12O2/c1-2-6-9(5-1)11-12-10-7-3-4-8-10/h1,7H,2-4,6,8H2); if there is a mismatch using OpenBabel approach, print a warning and go to the backup check using MoleCoor
    code was refactored to reduce repeated code
    also there was a minor change to whether InChI vs. modified InChI was used in certain parts of the code

---
+++ scripts/XYZtoMOLconn.py
@@ -0,0 +1,12 @@
+#uses MoleCoor
+#argv[1] = input (XYZ) file path
+#argv[2] = output (MOL) file name
+#argv[3] = molecule name
+#argv[4] = tolerance for connectivity perception
+#argv[5] = $RMG/source/MoleCoor path
+import sys
+sys.path.append(sys.argv[5])#add $RMG/source/MoleCoor to the PYTHONPATH so that import statements below work properly
+import MolecularCoordinates
+mg = MolecularCoordinates.readXYZFile(sys.argv[1])
+mg.perceiveConnectivity(tol=sys.argv[4])
+mg.writeMOLFile(sys.argv[2],sys.argv[3], connectivity=True)
