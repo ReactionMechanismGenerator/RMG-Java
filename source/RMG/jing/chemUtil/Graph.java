ce7c34b94efbde7b9220ff5deced5fe7218016b4
Connie Gao <connieg@mit.edu>
2014-01-26 19:09:23 -0500

    Added additional pi bond count for radicals in a cycle to fix aromaticity perception
    Add one to pi bond count if the atom is a radical and has only single bonds.

--- source/RMG/jing/chemUtil/Graph.java
+++ source/RMG/jing/chemUtil/Graph.java
@@ -672,6 +672,22 @@ public class Graph {
                 if (a.isOxygen() && node.getNeighborNumber() == 2)
                     numPiBonds = numPiBonds + 2;
                 // we dont have anionic carbon
+                if (a.isRadical()) {
+                	Iterator iter = node.getNeighbor();
+                	boolean onlySingleBonds = true;
+                	while (iter.hasNext()) {
+                		Arc arc = (Arc) iter.next();
+                		Bond b = (Bond) arc.getElement();
+                		if (b.isDouble() || b.isTriple()) {
+                			onlySingleBonds = false;
+                			break;
+                		}
+                	}
+                	if(onlySingleBonds) {
+                		numPiBonds = numPiBonds + 1;                	
+                	}
+                }
+                
             } else {
                 // if a double bond then 2 pi electrons and if a triple bond then 4 pi electrons
                 Arc a = (Arc) gc;
